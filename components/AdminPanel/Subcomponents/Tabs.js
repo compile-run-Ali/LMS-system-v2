286524031bf55eee130a0d26faaad62d374150ef
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-02-12 22:13:31 +0500

    Paper api changed, edit mcq updated

--- components/AdminPanel/Subcomponents/Tabs.js
+++ components/AdminPanel/Subcomponents/Tabs.js
@@ -7,7 +7,7 @@ export default function Tabs({active, setActive}) {
             <ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-primary-black">
                 <li className="mr-2 cursor-pointer" onClick={()=>setActive("Faculty")}>
                     <a href="#" className={`inline-flex p-4 border-b-2 border-transparent ${active === "Faculty" ?"text-blue-800 border-b-2 border-blue-800" :""} rounded-t-lg  group`}>
-                        <svg aria-hidden="true" className={`w-5 h-5 mr-2 ${active === "Faculty" ?"text-blue-800" :""} `} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>Faculty
+                        <svg aria-hidden="true" className={`w-5 h-5 mr-2 ${active === "Faculty" ?"text-blue-800" :""} `} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd"></path></svg>Faculty
                     </a>
                 </li>
                 <li className="mr-2 cursor-pointer" onClick={()=>setActive("Courses")}>
@@ -17,7 +17,7 @@ export default function Tabs({active, setActive}) {
                 </li>
                 <li className="mr-2 cursor-pointer" onClick={()=>setActive("Exams")} >
                     <a href="#" className={`inline-flex p-4 border-b-2 border-transparent rounded-t-lg group ${active === "Exams" ?"text-blue-800 border-b-2 border-blue-800" :""}`}>
-                        <svg aria-hidden="true" className={`w-5 h-5 mr-2 ${active === "Exams" ?"text-blue-800 " :""} `} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd"></path></svg>Exams
+                        <svg aria-hidden="true" className={`w-5 h-5 mr-2 ${active === "Exams" ?"text-blue-800 " :""} `} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fillRule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd"></path></svg>Exams
                     </a>
                 </li>
             </ul>
--- pages/api/faculty/paper_creation/new_paper.js
+++ pages/api/faculty/paper_creation/new_paper.js
@@ -7,11 +7,12 @@ const handler = async (req, res) => {
     //Create Paper and connect to course code then connect course with paperid
     const paper = await prisma.paper.create({
       data: {
-        paper_name: req.body.paperName,
-        time: req.body.paperTime,
+        paper_name: req.body.paper_name,
+        time: req.body.paper_time,
         date: req.body.date,
         duration: req.body.duration,
         weightage: req.body.weightage,
+        paper_type: req.body.paper_type,
       }
     })
     //connect course with paper Id
