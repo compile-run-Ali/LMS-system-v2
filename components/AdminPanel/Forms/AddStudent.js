31490764d9894c929c10e372b8d2210caa8465f9
Ali Abbas <maliabbas2001@gmail.com>
2023-02-17 20:16:49 +0500

    student self register implemented

--- components/AdminPanel/Forms/AddStudent.js
+++ components/AdminPanel/Forms/AddStudent.js
@@ -34,7 +34,7 @@ export default function AddStudent() {
 
   const addStudent = async (student) => {
     const new_student = await axios.post(
-      "http://localhost:3000/api/admin/student/add_student",
+      "/api/admin/student/add_student",
       {
         ...student,
       }
