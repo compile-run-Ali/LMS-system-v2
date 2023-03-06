589a855fa97a8d6f13e3ad260b5e4bd50fa6057a
Ali Abbas <maliabbas2001@gmail.com>
2023-03-06 12:35:32 +0500

    ui changes

--- components/Paper/Submitted.js
+++ components/Paper/Submitted.js
@@ -5,10 +5,10 @@ export default function Submitted() {
   const router = useRouter();
     const { student } = router.query;
   return (
-    <div className="flex flex-col items-center justify-center bg-zinc-700 p-8 rounded-lg">
+    <div className="flex flex-col items-center justify-center p-8 rounded-lg">
       <h1 className="text-4xl font-bold text-zinc-100">Paper Submitted</h1>
       <button
-        className="bg-zinc-100 text-zinc-700 font-bold py-2 px-4 rounded mt-6"
+        className="bg-zinc-100 text-blue-900 font-bold py-2 px-4 rounded mt-6"
         onClick={() => router.push(`/student`)}
       >
         Go Back
