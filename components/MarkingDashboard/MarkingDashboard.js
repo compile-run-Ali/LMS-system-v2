9aacbe7b401840b8805941c1ea9f88f98a840121
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2023-03-07 16:35:19 +0500

    ui fixed

--- components/MarkingDashboard/MarkingDashboard.js
+++ components/MarkingDashboard/MarkingDashboard.js
@@ -3,7 +3,7 @@ import StudentsTable from "./StudentsTable";
 
 const MarkingDashboard = ({ students_data, exam_id }) => {
   return (
-    <div>
+    <div className="w-11/12 mx-auto">
       <h1 className="text-2xl font-poppins font-bold">Marking Dashboard</h1>
       <StudentsTable students_data={students_data} exam_id={exam_id} />
     </div>
