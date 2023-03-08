cb15d02adbb4490b963c21f643d2972a7aa9d9c4
Ali Abbas <maliabbas2001@gmail.com>
2023-03-08 14:25:28 +0500

    bug fix

--- pages/faculty/exam_details/[exam_id].js
+++ pages/faculty/exam_details/[exam_id].js
@@ -1,14 +1,13 @@
 import BaseLayout from "@/components/BaseLayout/BaseLayout";
 import DashboardLayout from "@/components/DasboardLayout/DashboardLayout";
 import Exam from "@/components/Exam/Exam";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 
 export default function ExamPage() {
-
-  const router = useRouter()
-  const {exam_id} = router.query;
+  const router = useRouter();
+  const { exam_id } = router.query;
   const [examDetails, setExamDetails] = useState(null);
   const [objectiveQuestions, setObjectiveQuestions] = useState(null);
   const [subjectiveQuestions, setSubjectiveQuestions] = useState(null);
@@ -19,15 +18,15 @@ export default function ExamPage() {
     });
 
     setExamDetails(examDetails.data);
-  
+
     const objectiveQuestions = await axios.post("/api/faculty/get_objective", {
       paper_id: exam_id,
     });
 
     setObjectiveQuestions(objectiveQuestions.data);
-  
+
     let subjectiveQuestions = [];
-  
+
     if (examDetails.data.paper_type === "Subjective/Objective") {
       const res = await axios.post("/api/faculty/get_subjective", {
         paper_id: exam_id,
@@ -36,13 +35,14 @@ export default function ExamPage() {
     }
 
     setSubjectiveQuestions(subjectiveQuestions);
-  }
+  };
 
   useEffect(() => {
-    console.log(examDetails);
+    fetchExam();
+    console.log("exam details are", examDetails);
   }, []);
   return (
-    <BaseLayout title={"Exam | " + examDetails.paper_name}>
+    <BaseLayout title={"Exam | " + examDetails?.paper_title}>
       <DashboardLayout>
         {examDetails && (
           <Exam
