d10bd6a7d753d38d819bcb6c796ba16acd00cfd0
Ali Abbas <maliabbas2001@gmail.com>
2023-03-15 00:56:44 +0500

    removed/added logs

--- utils/FormatDate.js
+++ utils/FormatDate.js
@@ -1,29 +1,27 @@
 export const formatDate = (dateInString) => {
-    // date is in format   "2021-05-20T18:30:00.000Z"
-    const date = new Date(dateInString);
-    console.log("date is ", date);
+  // date is in format   "2021-05-20T18:30:00.000Z"
+  const date = new Date(dateInString);
 
-    const dateString =
-        date.getFullYear() +
-        "-" +
-        (date.getMonth() + 1).toString().padStart(2, "0") +
-        "-" +
-        date.getDate().toString().padStart(2, "0");
+  const dateString =
+    date.getFullYear() +
+    "-" +
+    (date.getMonth() + 1).toString().padStart(2, "0") +
+    "-" +
+    date.getDate().toString().padStart(2, "0");
 
-    return dateString;
+  return dateString;
 };
 
 export const formatTime = (dateInString) => {
-    // date is in format   "2021-05-20T18:30:00.000Z"
-    const date = new Date(dateInString);
-    console.log("date is ", date);
+  // date is in format   "2021-05-20T18:30:00.000Z"
+  const date = new Date(dateInString);
 
-    const timeString =
-        (date.getHours()-5).toString().padStart(2, "0") +
-        ":" +
-        date.getMinutes().toString().padStart(2, "0") +
-        ":" +
-        date.getSeconds().toString().padStart(2, "0");
+  const timeString =
+    (date.getHours() - 5).toString().padStart(2, "0") +
+    ":" +
+    date.getMinutes().toString().padStart(2, "0") +
+    ":" +
+    date.getSeconds().toString().padStart(2, "0");
 
-    return timeString;
-}
+  return timeString;
+};
