8bde3678cf9bedf7aefb905763418be93c847950
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2023-03-15 12:52:23 +0500

    add faculty picture-edit faculty removed

--- components/AdminPanel/Forms/AddFaculty.js
+++ components/AdminPanel/Forms/AddFaculty.js
@@ -2,6 +2,7 @@ import Input from "@/components/Common/Form/Input";
 import React, { useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
+import { headers } from "@/next.config";
 
 const AddFaculty = () => {
   const router = useRouter();
@@ -16,31 +17,34 @@ const AddFaculty = () => {
   );
   const [email, setEmail] = useState(edit ? router.query.email : "");
   const [password, setPassword] = useState("");
+  const [profilePicture, setProfilePicture] = useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    await addFaculty({
-      name,
-      phone_number: phoneNumber,
-      level,
-      department,
-      email,
-      password,
-    });
-    setName("");
-    setPhoneNumber("");
-    setLevel("");
-    setDepartment("");
-    setEmail("");
-    setPassword("");
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("phone_number", phoneNumber);
+    formData.append("level", level);
+    formData.append("department", department);
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("profile_picture", profilePicture);
+
+    await addFaculty(formData);
+  };
+  const handleFileChange = (event) => {
+    setProfilePicture(event.target.files[0]);
+    console.log(event.target.files[0]);
   };
 
   const addFaculty = async (faculty) => {
     const new_faculty = await axios.post(
-      `/api/admin/faculty/${edit ? "edit_faculty" : "add_faculty"}`,
+      `/api/admin/faculty/add_faculty`,
+      faculty,
       {
-        faculty_id: edit ? router.query.faculty_id : null,
-        ...faculty,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
       }
     );
     if (new_faculty.status === 200) {
@@ -106,7 +110,10 @@ const AddFaculty = () => {
         </div>
 
         <div className="font-poppins mt-4">
-          <label className="block mb-2  text-primary-black" htmlFor="file_input">
+          <label
+            className="block mb-2  text-primary-black"
+            htmlFor="file_input"
+          >
             Upload Profile Pic
           </label>
           <input
@@ -114,6 +121,8 @@ const AddFaculty = () => {
             aria-describedby="file_input_help"
             id="file_input"
             type="file"
+            accept="image/png, image/gif, image/jpeg"
+            onChange={handleFileChange}
           />
           <p className="mt-1 pl-1 text-sm text-black-100 " id="file_input_help">
             SVG, PNG, JPG or GIF (MAX. 800x400px).
--- pages/api/admin/faculty/add_faculty.js
+++ pages/api/admin/faculty/add_faculty.js
@@ -1,33 +1,85 @@
 import { PrismaClient } from "@prisma/client"
 import bcrypt from "bcrypt"
+import {IncomingForm} from "formidable"
+import mv from "mv"
+
+const prisma = new PrismaClient()
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
 
 const handler = async (req, res) => {
-  const prisma = new PrismaClient()
-  try {
-    //Create Faculty
-    const hash = await bcrypt.hash(req.body.password, 0)
-    const faculty = await prisma.faculty.create({
-      data: {
-        name: req.body.name,
-        email: req.body.email,
-        department: req.body.department,
-        password: hash,
-        level: Number(req.body.level),
-        phone_number: req.body.phone_number,
-      },
-      select: {
-        faculty_id: true,
-        name: true,
-        email: true,
-        department: true,
-        level: true,
-        phone_number: true,
-      },
+  const data = await new Promise((resolve, reject) => {
+    const form = new IncomingForm({multiples: true})
+    
+
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.error(err)
+        return reject(err)
+      }
+      try {
+        const hash = await bcrypt.hash(fields.password, 0)
+        const facultyData = {
+          name: fields.name,
+          email: fields.email,
+          department: fields.department,
+          password: hash,
+          level: Number(fields.level),
+          phone_number: fields.phone_number,
+        }
+
+        if (files.profile_picture) {
+          facultyData.profile_picture = files.profile_picture.originalFilename
+
+          // Move the uploaded file to the specified directory
+          const oldPath = files.profile_picture.filepath
+          const newPath = `./public/uploads/${files.profile_picture.originalFilename}`
+
+          mv(oldPath, newPath, function (err) {
+            if (err) {
+              console.error(err)
+              return reject(err)
+            }
+          
+            prisma.faculty.create({
+              data: facultyData,
+              select: {
+                faculty_id: true,
+                name: true,
+                email: true,
+                department: true,
+                level: true,
+                phone_number: true,
+                profile_picture: true,
+              },
+            }).then(resolve).catch(reject)
+          })
+        } else {
+          prisma.faculty.create({
+            data: facultyData,
+            select: {
+              faculty_id: true,
+              name: true,
+              email: true,
+              department: true,
+              level: true,
+              phone_number: true,
+            },
+          }).then(resolve).catch(reject)
+        }
+      } catch (err) {
+        console.error(err)
+        return reject(err)
+      }
     })
-    res.status(200).json(faculty)
-  } catch (err) {
-    throw new Error(err.message)
-  }
+  })
+
+  res.status(200).json(data)
+
 }
 
 export default handler
