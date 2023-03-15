8851dda3b73bafdeda3020b14af10d555f9769ee
Ali Abbas <maliabbas2001@gmail.com>
2023-03-15 12:44:38 +0500

    formatted

--- components/CreateExam/CreateExam.js
+++ components/CreateExam/CreateExam.js
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-import Wizard from '../Common/Wizard/Wizard'
-import Form from '../Common/Form/Form'
-import MCQTable from '../CreateObjective/ObjectiveExam'
-import Exam from '../Exam/Exam'
-import axios from 'axios'
-import SubjectiveExam from '../CreateSubjective/SubjectiveExam'
-
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Wizard from "../Common/Wizard/Wizard";
+import Form from "../Common/Form/Form";
+import MCQTable from "../CreateObjective/ObjectiveExam";
+import Exam from "../Exam/Exam";
+import axios from "axios";
+import SubjectiveExam from "../CreateSubjective/SubjectiveExam";
 
 const wizardItemsObjective = [
   {
@@ -20,8 +19,8 @@ const wizardItemsObjective = [
   {
     id: 3,
     title: "Exam Review",
-  }
-]
+  },
+];
 
 const wizardItemsSubjective = [
   {
@@ -39,42 +38,45 @@ const wizardItemsSubjective = [
   {
     id: 4,
     title: "Exam Review",
-  }
-]
-
+  },
+];
 
 export default function CreateExam({ paperType }) {
-  const router = useRouter()
+  const router = useRouter();
 
-  const [examDetails, setExamDetails] = useState(Object.keys(router.query).length > 1 ? router.query : null)
+  const [examDetails, setExamDetails] = useState(
+    Object.keys(router.query).length > 1 ? router.query : null
+  );
   const [active, setActive] = useState(1);
-  const [paperId, setPaperId] = useState(examDetails ? examDetails.paper_id : 0);
+  const [paperId, setPaperId] = useState(
+    examDetails ? examDetails.paper_id : 0
+  );
   const [exam, setExam] = useState();
   const [mcqs, setMCQs] = useState([]);
   const [subjectives, setSubjectives] = useState([]);
-  const [freeFlowGlobal, setFreeFlowGlobal] = useState(false)
+  const [freeFlowGlobal, setFreeFlowGlobal] = useState(false);
 
   const fetchExam = async () => {
     const res = await axios.post("/api/faculty/get_exam", {
-      paper_id: paperId
-    })
+      paper_id: paperId,
+    });
     setExam(res.data);
-  }
+  };
 
   const fetchObjectives = async () => {
     const res = await axios.post("/api/faculty/get_objective", {
-      paper_id: paperId
-    })
-    console.log(res.data)
+      paper_id: paperId,
+    });
+    console.log(res.data);
     setMCQs(res.data);
-  }
+  };
 
   const fetchSubjectives = async () => {
     const res = await axios.post("/api/faculty/get_subjective", {
-      paper_id: paperId
-    })
+      paper_id: paperId,
+    });
     setSubjectives(res.data);
-  }
+  };
 
   useEffect(() => {
     if (paperId !== 0 && !exam) {
@@ -89,39 +91,71 @@ export default function CreateExam({ paperType }) {
     }
   }, [paperId, exam, paperType]);
 
-
   return (
-    <div className='w-full pl-6 mt-2'>
-      <Wizard active={active} items={paperType === "Subjective/Objective" ? wizardItemsSubjective : wizardItemsObjective} />
-      {
-        active === 1 &&
-        <Form setActive={setActive} setPaperId={setPaperId} examDetails={examDetails} paperType={paperType} setFreeFlowGlobal={setFreeFlowGlobal}/>
-      }
-      {
-        active === 2 && paperId !== 0 &&
-        <div className='mt-10'>
-            <MCQTable paperId={paperId} setActive={setActive} objective_questions={mcqs} setObjectiveQuestions={ setMCQs} freeFlow={freeFlowGlobal}/>
+    <div className="w-full pl-6 mt-2">
+      <Wizard
+        active={active}
+        items={
+          paperType === "Subjective/Objective"
+            ? wizardItemsSubjective
+            : wizardItemsObjective
+        }
+      />
+      {active === 1 && (
+        <Form
+          setActive={setActive}
+          setPaperId={setPaperId}
+          examDetails={examDetails}
+          paperType={paperType}
+          setFreeFlowGlobal={setFreeFlowGlobal}
+        />
+      )}
+      {active === 2 && paperId !== 0 && (
+        <div className="mt-10">
+          <MCQTable
+            paperId={paperId}
+            setActive={setActive}
+            objective_questions={mcqs}
+            setObjectiveQuestions={setMCQs}
+            freeFlow={freeFlowGlobal}
+          />
         </div>
-      }
-      {
-        active === 3 && paperId !== 0 && paperType === "Objective" &&
-        <div className='mt-10'>
-            <Exam exam={exam} objectiveQuestions={mcqs} subjectiveQuestions={subjectives} isEdit={true}/>
+      )}
+      {active === 3 && paperId !== 0 && paperType === "Objective" && (
+        <div className="mt-10">
+          <Exam
+            exam={exam}
+            objectiveQuestions={mcqs}
+            subjectiveQuestions={subjectives}
+            isEdit={true}
+          />
         </div>
-      }
-      {
-        active === 3 && paperId !== 0 && paperType === "Subjective/Objective" &&
-        <div className='mt-10'>
-            <SubjectiveExam paperId={paperId} setActive={setActive} subjective_questions={subjectives} setSubjectiveQuestions={setSubjectives} />
-        </div>
-      }
-      {
-        active === 4 && paperId !== 0 && paperType === "Subjective/Objective" &&
-        <div className='mt-10'>
-            <Exam exam={exam} objectiveQuestions={mcqs} subjectiveQuestions={subjectives} isEdit={true}/>
-        </div>
-      }
+      )}
+      {active === 3 &&
+        paperId !== 0 &&
+        paperType === "Subjective/Objective" && (
+          <div className="mt-10">
+            <SubjectiveExam
+              paperId={paperId}
+              setActive={setActive}
+              subjective_questions={subjectives}
+              setSubjectiveQuestions={setSubjectives}
+            />
+          </div>
+        )}
+      {active === 4 &&
+        paperId !== 0 &&
+        paperType === "Subjective/Objective" && (
+          <div className="mt-10">
+            <Exam
+              exam={exam}
+              objectiveQuestions={mcqs}
+              subjectiveQuestions={subjectives}
+              isEdit={true}
+              setActive={setActive}
+            />
+          </div>
+        )}
     </div>
-
-  )
+  );
 }
