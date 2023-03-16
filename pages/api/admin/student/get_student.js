f95d9ad42b86842de132acace57eb6391f003a0f
Ali Abbas <maliabbas2001@gmail.com>
2023-03-16 20:21:54 +0500

    api fix

--- pages/api/admin/student/get_student.js
+++ pages/api/admin/student/get_student.js
@@ -4,7 +4,23 @@ const handler = async (req, res) => {
   const prisma = new PrismaClient();
   try {
     //  Find Student
-    const student = await prisma.student.findMany();
+    const student = await prisma.student.findMany({
+      select: {
+        p_number: true,
+        name: true,
+        email: true,
+        cgpa: true,
+        DOB: true,
+        password: true,
+        phone_number: true,
+        profile_picture: true,
+        courses: {
+          select: {
+            course_code: true,
+          },
+        },
+      },
+    });
     res.status(200).json(student);
   } catch (err) {
     throw new Error(err.message);
