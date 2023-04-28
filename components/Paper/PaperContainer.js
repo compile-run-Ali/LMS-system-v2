bb2ada6e5d403242446a605d6b89f6cc94d34b2f
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-04-28 13:44:19 +0500

    Fetching paper details multiple time

--- components/Paper/PaperContainer.js
+++ components/Paper/PaperContainer.js
@@ -57,11 +57,12 @@ export default function PaperContainer({ startOfPage }) {
     }
   }, [paper]);
 
-  useEffect(() => {
+  useEffect(() => () => {
     if (session.status === "authenticated") {
       setStudent(session.data.user.id);
     }
-  }, [session]);
+  });
+  [session];
 
   const shuffleArray = (array) => {
     const shuffledArray = [...array];
@@ -82,6 +83,132 @@ export default function PaperContainer({ startOfPage }) {
     papers[paper].current = newValue;
     localStorage.setItem("papers", JSON.stringify(papers));
   };
+
+  const fetchPaperDetails = async (papers) => {
+
+    const res = await axios.get(`/api/paper/${paper}`)
+    // subtract time here from startTime to get time taken in seconds
+    console.log("paper details from server", res.data);
+
+    if (res.status === 400) {
+      // paper not found
+      throw new Error("Paper not found");
+    }
+
+    // push the paper id in papers index array
+    const currentPaper = res.data;
+    // if paper is live get paper
+    if (currentPaper) {
+      papers[paper] = currentPaper;
+      localStorage.setItem("papers", JSON.stringify(papers));
+      setPaperDetails(res.data);
+
+      // get objective questions
+      axios
+        .get(`/api/student/paper/oq/${paper}`)
+        .then((res) => {
+          const randomizedQuestions = shuffleArray(res.data);
+          // set objective questions in array and local current, both in value of the paper_id key
+          papers[paper].objectiveQuestions = randomizedQuestions;
+          papers[paper].current = 0;
+          papers[paper].objectiveCount = randomizedQuestions.length;
+          localStorage.setItem("papers", JSON.stringify(papers));
+          setObjectiveCount(randomizedQuestions.length);
+          setLoading(false);
+          setQuestions(papers[paper].objectiveQuestions);
+        })
+        .catch((err) => {
+          console.log("error ", err.message);
+        });
+
+      // if paper is not objective
+      if (currentPaper.paper_type === "Subjective/Objective") {
+        // get subjective questions
+        axios
+          .get(`/api/student/paper/sq/${paper}`)
+          .then((res) => {
+            console.log(
+              "time taken to get subjective",
+              (new Date() - startOfPage) / 1000
+            );
+            const subjectiveQuestions = res.data.filter(
+              (question) => !question.parent_sq_id
+            );
+            papers[paper].subjectiveQuestions = subjectiveQuestions;
+            localStorage.setItem("papers", JSON.stringify(papers));
+            setQuestions(
+              [
+                ...papers[paper].objectiveQuestions,
+                ...papers[paper].subjectiveQuestions,
+              ] || []
+            );
+          })
+          .catch((err) => {
+            console.log("error ", err.message);
+          });
+      }
+      if (currentPaper.paper_type === "IE") {
+        axios
+          .get(`/api/faculty/get_ie_files`, {
+            params: {
+              paperId: paper,
+            },
+          })
+          .then((res) => {
+            console.log("time taken to get ie", res.data);
+
+            // Make second API call using response of first
+            //extract file urls from response
+            console.log("Response from first API call", res.data);
+            //loop through the data array and make a post request for each file url
+            res.data.ie_questions.forEach((file) => {
+              axios
+                .post(`/api/faculty/read_file`, {
+                  fileUrl: file.fileUrl,
+                })
+                .then((res) => {
+                  console.log(
+                    "Response from second API call",
+                    res.data
+                  );
+                  //loop through the res.data and if the data is filled, excelData
+                  res.data.forEach((data) => {
+                    if (data) {
+                      setExcelData((excelData) => [...excelData, data]);
+                    }
+                  });
+                })
+                .catch((error) => {
+                  console.error("Error in second API call", error);
+                });
+            });
+          })
+          .catch((error) => {
+            console.error("Error in first API call", error);
+          });
+      }
+    }
+    // if paper is not live, push back to papers list
+    else {
+      router.push(`/student`);
+    }
+
+    //update spa status to Attempted
+    const startTime = new Date();
+    // get gmt offset in hours, and add that in startTime
+    const gmtOffset = new Date().getTimezoneOffset();
+    startTime.setMinutes(startTime.getMinutes() - gmtOffset);
+
+    const updated = await axios
+      .post(`/api/student/paper/update_attempt_status`, {
+        studentId: session.data.user.id,
+        paperId: paper,
+        status: "Attempted",
+        timeStarted: startTime.toISOString(),
+      })
+
+    console.log("updated attempt status ", updated.data);
+  }
 
   useEffect(() => {
     setCount(count + 1);
@@ -112,132 +239,21 @@ export default function PaperContainer({ startOfPage }) {
         );
 
         // get paper details
-        axios
-          .get(`/api/paper/${paper}`)
-          .then((res) => {
-            // subtract time here from startTime to get time taken in seconds
-            console.log("paper details from server", res.data);
-            // push the paper id in papers index array
-            const currentPaper = res.data;
-            // if paper is live get paper
-            if (currentPaper) {
-              papers[paper] = currentPaper;
-              localStorage.setItem("papers", JSON.stringify(papers));
-              setPaperDetails(res.data);
+        try {
+          fetchPaperDetails(papers)
+        }
+        catch (err) {
+          console.log("error in fetching paper details", err.message);
+          try {
+            setInterval(() => {
+              fetchPaperDetails(papers);
+            }, 5000);
+          } catch (err) {
+            console.log("Still can not fetch paper details", err.message);
+          }
 
-              // get objective questions
-              axios
-                .get(`/api/student/paper/oq/${paper}`)
-                .then((res) => {
-                  const randomizedQuestions = shuffleArray(res.data);
-                  // set objective questions in array and local current, both in value of the paper_id key
-                  papers[paper].objectiveQuestions = randomizedQuestions;
-                  papers[paper].current = 0;
-                  papers[paper].objectiveCount = randomizedQuestions.length;
-                  localStorage.setItem("papers", JSON.stringify(papers));
-                  setObjectiveCount(randomizedQuestions.length);
-                  setLoading(false);
-                  setQuestions(papers[paper].objectiveQuestions);
-                })
-                .catch((err) => {
-                  console.log("error ", err.message);
-                });
+        }
 
-              // if paper is not objective
-              if (currentPaper.paper_type === "Subjective/Objective") {
-                // get subjective questions
-                axios
-                  .get(`/api/student/paper/sq/${paper}`)
-                  .then((res) => {
-                    console.log(
-                      "time taken to get subjective",
-                      (new Date() - startOfPage) / 1000
-                    );
-                    const subjectiveQuestions = res.data.filter(
-                      (question) => !question.parent_sq_id
-                    );
-                    papers[paper].subjectiveQuestions = subjectiveQuestions;
-                    localStorage.setItem("papers", JSON.stringify(papers));
-                    setQuestions(
-                      [
-                        ...papers[paper].objectiveQuestions,
-                        ...papers[paper].subjectiveQuestions,
-                      ] || []
-                    );
-                  })
-                  .catch((err) => {
-                    console.log("error ", err.message);
-                  });
-              }
-              if (currentPaper.paper_type === "IE") {
-                axios
-                  .get(`/api/faculty/get_ie_files`, {
-                    params: {
-                      paperId: paper,
-                    },
-                  })
-                  .then((res) => {
-                    console.log("time taken to get ie", res.data);
-
-                    // Make second API call using response of first
-                    //extract file urls from response
-                    console.log("Response from first API call", res.data);
-                    //loop through the data array and make a post request for each file url
-                    res.data.ie_questions.forEach((file) => {
-                      axios
-                        .post(`/api/faculty/read_file`, {
-                          fileUrl: file.fileUrl,
-                        })
-                        .then((res) => {
-                          console.log(
-                            "Response from second API call",
-                            res.data
-                          );
-                          //loop through the res.data and if the data is filled, excelData
-                          res.data.forEach((data) => {
-                            if (data) {
-                              setExcelData((excelData) => [...excelData, data]);
-                            }
-                          });
-                        })
-                        .catch((error) => {
-                          console.error("Error in second API call", error);
-                        });
-                    });
-                  })
-                  .catch((error) => {
-                    console.error("Error in first API call", error);
-                  });
-              }
-            }
-            // if paper is not live, push back to papers list
-            else {
-              router.push(`/student`);
-            }
-          })
-          .catch((err) => {
-            console.log("error ", err.message);
-          });
-
-        //update spa status to Attempted
-        const startTime = new Date();
-        // get gmt offset in hours, and add that in startTime
-        const gmtOffset = new Date().getTimezoneOffset();
-        startTime.setMinutes(startTime.getMinutes() - gmtOffset);
-
-        axios
-          .post(`/api/student/paper/update_attempt_status`, {
-            studentId: session.data.user.id,
-            paperId: paper,
-            status: "Attempted",
-            timeStarted: startTime.toISOString(),
-          })
-          .then((res) => {
-            console.log("updated attempt status ", res.data);
-          })
-          .catch((err) => {
-            console.log("error updating attempt status", err);
-          });
       }
     }
   }, [paper, student]);
@@ -252,7 +268,7 @@ export default function PaperContainer({ startOfPage }) {
     <div className="flex justify-between shadow-lg max-w-5xl font-poppins mt-28 mx-20 xl:mx-auto pt-20 pb-10 px-10 gradient rounded-2xl shadow-3xl shadow-black">
       <div className="w-2/3  rounded-l-2xl">
         {currentQuestion === questions.length &&
-        paperDetails.paper_type !== "IE" ? (
+          paperDetails.paper_type !== "IE" ? (
           <Submitted />
         ) : paperDetails.paper_type === "Objective" ||
           paperDetails.paper_type === "Word" ? (
