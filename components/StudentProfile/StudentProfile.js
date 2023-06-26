423697244092817fcecb168d2163e8bec448849f
compile-run-ali <aabdullah.bscs20seecs@seecs.edu.pk>
2023-06-26 13:21:26 +0500

    enroll and unenroll

--- components/StudentProfile/StudentProfile.js
+++ components/StudentProfile/StudentProfile.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { MdDelete } from "react-icons/md";
 
 export default function StudentProfile({ student }) {
   const router = useRouter();
-
+  const [courses, setCourses] = useState([]);
   const handleChangePassword = () => {
     router.push({
       pathname: "/change_password",
@@ -15,6 +18,47 @@ export default function StudentProfile({ student }) {
       },
     });
   };
+  async function getAllCourses() {
+    try {
+      const res = await axios.get("/api/admin/course/get_courses");
+      setCourses(res.data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  useEffect(() => {
+    getAllCourses();
+  }, []);
+  // p_number: pNumber,
+  // course_code: selectedCourse,
+  async function handleEnroll (course_code) {
+    try {
+      const res = await axios.post("/api/student/register", {
+        p_number: student.p_number,
+        course_code: course_code,
+      });
+      console.log(res.data);
+      alert("Course enrolled successfully");
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  async function handleUnenroll (course_code) {
+    try {
+      const res = await axios.post("/api/student/unregister", {
+        p_number: student.p_number,
+        course_code: course_code,
+      });
+      alert("Course unenrolled successfully");
+      console.log(res.data);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
 
   return (
     <>
@@ -30,6 +74,7 @@ export default function StudentProfile({ student }) {
             <p className="mb-2">
               <strong>Email:</strong> {student.email || "N/A"}
             </p>
+
             <p className="mb-2">
               <strong>Phone Number:</strong> {student.phone_number}
             </p>
@@ -47,7 +92,6 @@ export default function StudentProfile({ student }) {
               Change password
             </p>
           </div>
-
           <div className="w-40 aspect-square rounded-full border border-blue-900 absolute top-[76px] right-6">
             <Image
               src={`/uploads/${student.image ? student.image : "noface.jpeg"}`}
@@ -55,6 +99,67 @@ export default function StudentProfile({ student }) {
               className="rounded-full object-cover object-center"
               alt="user"
             />
+          </div>
+          <div className=" float-left m-4">
+            <p className="font-bold text-lg">
+              <strong>Courses available:</strong>
+            </p>
+            <div>
+              {courses.length > 0 ? (
+                <table className="table mt-2">
+                  <thead>
+                    <tr>
+                      <th>Course Code</th>
+                      <th>Status</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {courses.map((course) => (
+                      <tr key={course.id} className="text-center border border-black">
+                        <td className="p-4 border border-black">{course.course_code}</td>
+                        <td className="border border-black">
+                          {student.courses.some(
+                            (studentCourse) =>
+                              studentCourse.course_code === course.course_code
+                          ) ? (
+                            <button className="text-green-700 mx-7" disabled>
+                              Enrolled
+                            </button>
+                          ) : (
+                            <button
+                              className=" mx-7"
+                              onClick={() => handleEnroll(course.course_code)}
+                            >
+                              Enroll
+                            </button>
+                          )}
+                        </td>
+                        <td>
+                          {student.courses.some(
+                            (studentCourse) =>
+                              studentCourse.course_code === course.course_code
+                          ) ? (
+                            <button
+                              className="text-red-500"
+                              onClick={() => handleUnenroll(course.course_code)}
+                            >
+                              <MdDelete />
+                            </button>
+                          ) : (
+                            <button className="text-blue-500" disabled>
+                              <MdDelete />
+                            </button>
+                          )}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              ) : (
+                "N/A"
+              )}
+            </div>
           </div>
         </div>
       ) : (
--- pages/student/profile.js
+++ pages/student/profile.js
@@ -4,14 +4,26 @@ import { useSession } from "next-auth/react";
 import BaseLayout from "@/components/BaseLayout/BaseLayout";
 import DashboardLayout from "@/components/DasboardLayout/DashboardLayout";
 import StudentProfile from "@/components/StudentProfile/StudentProfile";
+import axios from "axios";
 
 export default function Profile() {
   const { data: session, status } = useSession();
   const [student, setStudent] = useState(null);
 
+  async function getStudent() {
+    try {
+      const res = await axios.get("/api/admin/student/get_student");
+      //store that student which is in session according to id
+      const student = res.data.find((student) => student.p_number === session?.user.id);
+      setStudent(student);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
   useEffect(() => {
     if (session) {
-      setStudent(session?.user);
+      getStudent();
     }
   }, [session]);
 
